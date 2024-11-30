b5b616a85c6339077ed67b14d1302c05a10cf9a6
Wesley Chou <wesleychou9851@gmail.com>
2024-11-29 17:02:38 -0800

    add hot reload on dev

--- app/main.js
+++ app/main.js
@@ -1,5 +1,14 @@
 const { app, BrowserWindow } = require('electron')
 
+const path = require('path') 
+const env = process.env.NODE_ENV || 'development';
+if (env === 'development') { 
+    require('electron-reload')(__dirname, { 
+        electron: path.join(__dirname, 'node_modules', '.bin', 'electron'), 
+        hardResetMethod: 'exit'
+    }); 
+} 
+
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
