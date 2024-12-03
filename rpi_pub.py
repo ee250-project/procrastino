27a0e799dfddbcfc330900dbb445715431fb22c6
Wesley Chou <wesleychou9851@gmail.com>
2024-12-02 20:47:57 -0800

    test

--- rpi_pub.py
+++ rpi_pub.py
@@ -40,27 +40,29 @@ if __name__ == '__main__':
         
         print("while loop started")
         try:
+            print("enter try")
             # Read distance value from Ultrasonic and potentiometer
             distance = grovepi.ultrasonicRead(ultrasonic_ranger)
             potentiometer_value = grovepi.analogRead(potentiometer)
+            print("read")
+            
+            # # Mapping the potentiometer value to the ultrasonic range
+            # threshold = 30
 
-            # Mapping the potentiometer value to the ultrasonic range
-            threshold = 30
+            # if threshold >= 30:
+            #     threshold = 30
 
-            if threshold >= 30:
-                threshold = 30
-
-            if distance < threshold:
-                setRGB(255, 0, 0)
-                count = 1
-                setText_norefresh(str(int(threshold)) + "cm Too close\n" + str(distance) + "cm")
-            elif distance > threshold and count == 1:
-                setRGB(0, 255, 0)
-                setText(str(int(threshold)) + "cm\n" + str(distance) + "cm")
-                count = 0
-            else:
-                setRGB(0, 255, 0)
-                setText_norefresh(str(int(threshold)) + "cm\n" + str(distance) + "cm")
+            # if distance < threshold:
+            #     setRGB(255, 0, 0)
+            #     count = 1
+            #     setText_norefresh(str(int(threshold)) + "cm Too close\n" + str(distance) + "cm")
+            # elif distance > threshold and count == 1:
+            #     setRGB(0, 255, 0)
+            #     setText(str(int(threshold)) + "cm\n" + str(distance) + "cm")
+            #     count = 0
+            # else:
+            #     setRGB(0, 255, 0)
+            #     setText_norefresh(str(int(threshold)) + "cm\n" + str(distance) + "cm")
 
             # Prepare data in JSON format
             data = {
